4208491a4185a7ad6c028387c6daf2a5208226a3
Greg Block <ggblock@wisc.edu>
2013-10-09 22:50:50 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VisibleMiniProject::Application.config.secret_token = '29ad8c050e0366c64f9c26f5d988050df36a58ebe3834e7f5a5fd0773bc0a1a3650333d5b154019ae1bd54db58c5aa3acb1d62c002a14eaf3804dc5e8ca38749'
