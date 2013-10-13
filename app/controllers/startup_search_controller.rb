61b2181f161934b0592de2a473ba7d5792bd572c
Greg Block <ggblock@wisc.edu>
2013-10-13 11:14:44 -0500

    refactored

--- app/controllers/startup_search_controller.rb
+++ app/controllers/startup_search_controller.rb
@@ -3,27 +3,19 @@ class StartupSearchController < ApplicationController
 
 
   def index
-    @startup = AngelListStartup.create
   end
 
   def search
     original_url = params[:url]
-    @startup = nil
-    original_url = cleanUrl(original_url)
-    
-    startup_url = original_url.split('.')[0]
-    startups = AngelListStartup.search_al(startup_url)
-    startups.each do |su|
-      startup = AngelListStartup.find_startup(su["id"])
-      company_url = cleanUrl(startup.company_url)
 
-      if company_url == original_url
-        @startup = startup
-        break
-      end
-    end
-    if @startup.blank?
-      redirect_to root_path, flash: {error: "Could not find Startup"}
+    original_url = cleanUrl(original_url)
+    startup_response = AngelListStartup.search_al(original_url)
+    if startup_response.success?
+      @startup = AngelListStartup.create(name: startup_response["name"],logo: startup_response["logo_url"], locations: startup_response["locations"],
+               product_description: startup_response["product_desc"],pitch: startup_response["high_concept"],markets: startup_response["markets"],
+               al_url: startup_response["angellist_url"], company_url: startup_response["company_url"] )
+    else
+      redirect_to root_path, flash: {error: "Cannot find Startup"}
     end
   end
 
--- app/models/angel_list_startup.rb
+++ app/models/angel_list_startup.rb
@@ -3,29 +3,15 @@ class AngelListStartup < ActiveRecord::Base
   include HTTParty
 
 
-  base_uri "https://api.angel.co/1/search?query="
+  base_uri "https://api.angel.co/1/startups/search.json?domain="
+
 
 
   attr_accessible :name, :logo, :locations, :product_description, :pitch, :markets, :al_url, :company_url
   
 
   def self.search_al(name)
-    response_str = name + "&type=Startup"
-    response = get(response_str)
+    response = get(name)
   end
-
-
-  def self.find_startup(id)
-    response_str = "https://api.angel.co/1/startups/#{id}"
-    response = get(response_str)
-    if response.success?
-      self.new(name: response["name"],logo: response["logo_url"], locations: response["locations"],
-               product_description: response["product_desc"],pitch: response["high_concept"],markets: response["markets"],
-               al_url: response["angellist_url"], company_url: response["company_url"] )
-    else
-      raise response.response
-    end
-  end
-
 
 end
